6483328d505c880d8b3df98915a69abc3222fd2e
shahzaib <shahzaib@gmail.com>
2021-12-03 17:10:32 +0500

    DM working acurately

--- src/main/scala/caches/DMCache.scala
+++ src/main/scala/caches/DMCache.scala
@@ -1,23 +1,53 @@
-// package caches
+package caches
 
-// import chisel3._ 
-// import chisel3.util._ 
+import chisel3._ 
+import chisel3.util._ 
 
-// class DMCache extends Module {
-//     val io = IO(new Bundle{
-//         val adr = Input(UInt(2.W))
-//         val wr_en = Input(Bool())
-//         val data_in = Input(UInt(32.W))
-//         val data_out = Output(UInt(32.W))
-//     })
+class DMCache extends Module {
+    val io = IO(new Bundle{
+        val adr = Input(UInt(4.W))
+        val wr_en = Input(Bool())
+        val data_in = Input(UInt(32.W))
+        val data_out = Output(UInt(32.W))
+        val miss = Output(Bool())
+    })
 
-//     // val mem = SyncReadMem(1024, UInt(32.W))
-//     // val cache_ind = SyncReadMem(16, Valid(UInt(4.W)))
+    val mem = SyncReadMem(16, UInt(32.W))
+    val cache_valid = SyncReadMem(4, Bool())
+    val cache_tags = SyncReadMem(4,UInt(2.W))
+    val cache_data = SyncReadMem(4,UInt(32.W))
 
-//     // for(i <- 0 to 16){
-//     //     cache_ind(i).valid := false.B
-//     // }
+    for(i <- 0 to 3){
+        cache_valid.write(i.U(2.W),false.B)
+    }
 
-//     io.data_out := 0.U
+    val data = WireInit(0.U(32.W))
 
-// }
+    io.miss := true.B
+
+    when(io.wr_en === true.B){         
+        mem.write(io.adr, io.data_in)
+        io.miss := true.B
+    }.otherwise{
+
+        when(cache_valid.read(io.adr(1,0)) && cache_tags.read(io.adr(1,0)) === io.adr(3,2)){
+                
+                io.data_out := cache_data(io.adr(1,0))
+                io.miss := false.B
+
+        }.otherwise{
+
+                
+                data := mem.read(io.adr)
+                cache_valid.write(io.adr(1,0), true.B)
+                cache_tags.write(io.adr(1,0), io.adr(3,2))
+                cache_data.write(io.adr(1,0), data)
+                io.miss := true.B
+                
+        }
+    }
+
+
+    io.data_out := data
+
+}
--- src/test/scala/caches/DMCacheTest.scala
+++ src/test/scala/caches/DMCacheTest.scala
@@ -1,20 +1,30 @@
-// package caches
+package caches
 
-// import org.scalatest.FreeSpec
-// import chisel3._ 
-// import chiseltest._
-// import chiseltest.ChiselScalatestTester
-// import chiseltest.internal.VerilatorBackendAnnotation
-// import chiseltest.experimental.TestOptionBuilder._
+import org.scalatest.FreeSpec
+import chisel3._ 
+import chiseltest._
+import chiseltest.ChiselScalatestTester
+import chiseltest.internal.VerilatorBackendAnnotation
+import chiseltest.experimental.TestOptionBuilder._
 
 
-// class DMCacheTest extends FreeSpec with ChiselScalatestTester {
-//     "Index Test" in {
-//         test(new DMCache).withAnnotations(Seq(VerilatorBackendAnnotation)){ c =>
-//             c.io.adr.poke(12.U)
-//             c.io.wr_en.poke(true.B)
-//             c.io.data_in.poke(4.U)
-//             c.clock.step(10)
-//         }
-//     }
-// }
+class DMCacheTest extends FreeSpec with ChiselScalatestTester {
+    "Index Test" in {
+        test(new DMCache).withAnnotations(Seq(VerilatorBackendAnnotation)){ c =>
+            c.io.adr.poke(12.U)
+            c.io.wr_en.poke(true.B)
+            c.io.data_in.poke(4.U)
+            c.clock.step(2)
+
+            c.io.adr.poke(12.U)
+            c.io.wr_en.poke(false.B)
+            c.io.data_in.poke(0.U)
+            c.clock.step(2)
+
+            c.io.adr.poke(12.U)
+            c.io.wr_en.poke(false.B)
+            c.io.data_in.poke(0.U)
+            c.clock.step(2)
+        }
+    }
+}
