07abbe10ad65ed29fbb89334b4dcb8c5186cc2d9
shahzaibk23 <shahzaibceo@gmail.com>
2022-03-26 23:57:38 +0500

    cache scheme updated

--- src/main/scala/caches/DMCache.scala
+++ src/main/scala/caches/DMCache.scala
@@ -2,67 +2,57 @@ package chisel_caches.caches
 
 import chisel3._ 
 import chisel3.util._ 
+// import math
 import jigsaw.rams.fpga.BlockRamWithMasking
 
-class DMCache(DW:Int, AW:Int/*, mainMem:BlockRamWithMasking*/) extends Module {
+class DMCache(cacheAddrWidth:Int, dataAddrWidth:Int, dataWidth:Int) extends Module {
     val io = IO(new Bundle{
-        val adr = Input(UInt(AW.W))
+        val adr = Input(UInt(dataAddrWidth.W))
         val wr_en = Input(Bool())
-        val data_in = Input(UInt(DW.W))
-        val data_out = Output(UInt(DW.W))
+        val data_in = Input(UInt(dataWidth.W))
+        val data_out = Output(UInt(dataWidth.W))
         val miss = Output(Bool())
     })
 
-    val cache_width = log2Ceil(AW)
-    val cache_half_width = cache_width/2
-
+   val cacheRows = (math.pow(2,cacheAddrWidth)).toInt
+    
     // val mem = Module(mainMem) //SyncReadMem(cache_width, UInt(32.W))
-    val cache_valid = SyncReadMem(cache_width, Bool())    // VALID
-    val cache_tags = SyncReadMem(cache_width,UInt(2.W))   // TAGS
-    val cache_data = SyncReadMem(cache_width,UInt(32.W))  // DATA
+    val cache_valid = SyncReadMem(cacheRows, Bool())    // VALID
+    val cache_tags = SyncReadMem(cacheRows,UInt((dataAddrWidth - cacheAddrWidth).W))   // TAGS
+    val cache_data = SyncReadMem(cacheRows,UInt(dataWidth.W))  // DATA
 
-    for(i <- 0 to cache_width.toInt-1){
-        cache_valid.write(i.U(log2Ceil(cache_width).W),false.B)
+    for(i <- 0 to cacheRows.toInt-1){
+        cache_valid.write(i.U(cacheAddrWidth.W),false.B)
     }
 
-    val data = WireInit(0.U(32.W))
+    val data = WireInit(0.U(dataWidth.W))
+    val miss = WireInit(true.B)
 
-    io.miss := true.B
 
     when(io.wr_en === true.B){         // write req
-        // mem.write(io.adr, io.data_in)
-        // mem.io.req.bits.addrRequest := io.adr
-        // mem.io.req.bits.activeByteLane := "b1111".U
-        // mem.io.req.bits.isWrite := true.B
-        // mem.io.req.bits.dataRequest := io.data_in
-        // mem.io.req.valid := true.B
-        io.miss := true.B
+        miss := true.B
+        cache_valid(io.adr(cacheAddrWidth, 0)) := true.B
+        cache_tags(io.adr(cacheAddrWidth, 0)) := io.adr(dataAddrWidth-1,cacheAddrWidth+1)
+        cache_data(io.adr(cacheAddrWidth, 0)) := io.data_in
     }.otherwise{
 
         // CACHE HIT
-        when(cache_valid.read(io.adr(cache_half_width,0)) && cache_tags.read(io.adr(cache_half_width,0)) === io.adr(cache_width-1,cache_width-2)){
+        when(cache_valid.read(io.adr(cacheAddrWidth,0)) && cache_tags.read(io.adr(cacheAddrWidth,0)) === io.adr(dataAddrWidth-1,cacheAddrWidth+1)){
                 
-                data := cache_data(io.adr(cache_half_width,0))
-                io.miss := false.B
+                data := cache_data(io.adr(cacheAddrWidth,0))
+                miss := false.B
 
         // CACHE MISS
         }.otherwise{
 
-                // mem.io.req.bits.addrRequest := io.adr
-                // mem.io.req.bits.activeByteLane := "b1111".U
-                // mem.io.req.bits.isWrite := false.B
-                // mem.io.req.bits.dataRequest := io.data_in
-                // mem.io.req.valid := true.B
-                // data := //Mux(mem.io.rsp.valid, mem.io.rsp.bits.dataResponse, 0.U) //mem.read(io.adr)
-                // cache_valid.write(io.adr(cache_half_width,0), true.B)
-                // cache_tags.write(io.adr(cache_half_width,0), io.adr(cache_width-1,cache_width-2))
-                // cache_data.write(io.adr(cache_half_width,0), data)
-                io.miss := true.B
+                // TO BE IMPLEMENTED
+                miss := true.B
                 
         }
     }
 
 
     io.data_out := data
+    io.miss := miss
 
 }
