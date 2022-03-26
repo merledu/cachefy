5cb58c1ed88d1b29e0fc365d5d51eff132787378
shahzaibk23 <shahzaibceo@gmail.com>
2022-03-26 23:58:40 +0500

    cache test updated

--- src/test/scala/caches/DMCacheTest.scala
+++ src/test/scala/caches/DMCacheTest.scala
@@ -10,7 +10,7 @@ import chiseltest.experimental.TestOptionBuilder._
 
 class DMCacheTest extends FreeSpec with ChiselScalatestTester {
     "Direct Mapped Cache Test" in {
-        test(new DMCache(32,128)).withAnnotations(Seq(VerilatorBackendAnnotation)){ c =>
+        test(new DMCache(2,16,32)).withAnnotations(Seq(VerilatorBackendAnnotation)){ c =>
             c.io.adr.poke(4.U)
             c.io.wr_en.poke(true.B)
             c.io.data_in.poke(20.U)
