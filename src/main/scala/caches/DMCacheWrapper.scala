cc557f3c00245631bd37d8919047bbf0a6a1c546
shahzaibk23 <shahzaibceo@gmail.com>
2022-03-26 23:58:47 +0500

    Wrapper modified

--- src/main/scala/caches/DMCacheWrapper.scala
+++ src/main/scala/caches/DMCacheWrapper.scala
@@ -15,39 +15,40 @@ class DMCacheWrapper[A <: AbstrRequest, B <: AbstrResponse]
     })
 
   //TODO: MAKE ROWS AND COLS DYNAMIC
-    val cache = Module(new DMCache(32,1024/*, mainMem*/))
-
+    val cache = Module(new DMCache(10,32,32/*, mainMem*/))
     val validReg = RegInit(false.B)
+    val dataReg = RegInit(0.U)
+    
+    when(io.reqIn.fire() && !io.reqIn.bits.isWrite) {
+      // READ
+      cache.io.adr := io.reqIn.bits.addrRequest
+      cache.io.wr_en := false.B
+      cache.io.data_in := 0.U
+      dataReg := cache.io.data_out 
+      validReg := true.B
+    } .elsewhen(io.reqIn.fire() && io.reqIn.bits.isWrite) {
+      // WRITE
+      cache.io.adr := io.reqIn.bits.addrRequest
+      cache.io.wr_en := true.B
+      cache.io.data_in := io.reqIn.bits.dataRequest
+      validReg := true.B
+      // io.rspOut.bits.dataResponse := DontCare
+    } .otherwise {
+      cache.io.adr := 0.U
+      cache.io.wr_en := false.B
+      cache.io.data_in := 0.U
+      validReg := false.B
+      // io.rspOut.bits.dataResponse := DontCare
+    }
 
     io.rspOut.valid := validReg
     io.rspOut.bits.error := false.B
     io.reqIn.ready := true.B // assuming we are always ready to accept requests from device
 
-    io.rspOut.bits.dataResponse := Mux(io.rspIn.valid, io.rspIn.bits.dataResponse, DontCare)
+    io.rspOut.bits.dataResponse := Mux(io.rspIn.valid, io.rspIn.bits.dataResponse, dataReg)
 
-    when(io.reqIn.fire() && !io.reqIn.bits.isWrite) {
-    // READ
-    cache.io.adr := io.reqIn.bits.addrRequest
-    cache.io.wr_en := false.B
-    cache.io.data_in := 0.U
-    io.rspOut.bits.dataResponse := cache.io.data_out 
-    validReg := true.B
-  } .elsewhen(io.reqIn.fire() && io.reqIn.bits.isWrite) {
-    // WRITE
-    cache.io.adr := io.reqIn.bits.addrRequest
-    cache.io.wr_en := true.B
-    cache.io.data_in := io.reqIn.bits.dataRequest
-    validReg := true.B
-    // io.rspOut.bits.dataResponse := DontCare
-  } .otherwise {
-    cache.io.adr := 0.U
-    cache.io.wr_en := false.B
-    cache.io.data_in := 0.U
-    validReg := false.B
-    // io.rspOut.bits.dataResponse := DontCare
-  }
 
-  io.reqOut.bits <> io.reqIn.bits
-  io.reqOut.valid := ~cache.io.miss
+    io.reqOut.bits <> io.reqIn.bits
+    io.reqOut.valid := ~cache.io.miss
 
 }
