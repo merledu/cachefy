1a071e32d13f665f68ccb720d422fc5b90f7806a
shahzaibk23 <shahzaibceo@gmail.com>
2023-04-21 23:13:11 +0500

    dummy tile structured

--- src/main/scala/Tile.scala
+++ src/main/scala/Tile.scala
@@ -2,32 +2,35 @@ import chisel3._
 import chisel3.util._
 
 import common._
-import caravan.bus.common.{AbstrRequest, AbstrResponse, AbstractAdapter, BusConfig}
+import directmapped.DMCache
+import caravan.bus.common.{AbstrRequest, AbstrResponse, AbstractAdapter, AbstractCore, BusConfig}
 
-class Tile[req:AbstrRequest, rsp:AbstrResponse, adapter:AbstractAdapter](val core: Module)(implicit val config: CacheConfig, implicit val busConfig: BusConfig) extends Module {
+class Tile[A <:AbstrRequest, B <:AbstrResponse, C <:AbstractAdapter[A,B], D <: AbstractCore[A,B]](val core_ : D,val request: A,val response:B,val adapter:C )(implicit val config: CacheConfig, implicit val busConfig: BusConfig) extends Module {
 
   val io = IO(new Bundle {
-    val req   = Decoupled(new req())            // request goes out to the SoC
-    val rsp   = Flipped(Decoupled(new rsp()))   // response comes back from the SoC
+    val req   = Decoupled(request)            // request goes out to the SoC
+    val rsp   = Flipped(Decoupled(response))   // response comes back from the SoC
   })
 
   val icache = Module(new DMCache)
   val dcache = Module(new DMCache)
 
-  val icache_adapter = Module(new adapter())
-  val dcache_adapter = Module(new adapter())
+  val icache_adapter = Module(adapter)
+  val dcache_adapter = Module(adapter)
+
+  val core = Module(core_)
 
   // core <-> icache_adapter <-> icache
   icache_adapter.io.reqIn <> core.io.imemReq
   core.io.imemRsp <> icache_adapter.io.rspOut
   icache.io.req <> icache_adapter.io.reqOut
-  icache_adapter.io.rspIn <> icache.io.rsp.asTypeOf(new rsp)
+  icache_adapter.io.rspIn <> icache.io.rsp.asTypeOf(response)
 
   // core <-> dcache_adapter <-> dcache
   dcache_adapter.io.reqIn <> core.io.dmemReq
   core.io.dmemRsp <> dcache_adapter.io.rspOut
   dcache.io.req <> dcache_adapter.io.reqOut
-  dcache_adapter.io.rspIn <> dcache.io.rsp.asTypeOf(new rsp)
+  dcache_adapter.io.rspIn <> dcache.io.rsp.asTypeOf(response)
  
 
   
--- src/test/scala/TileTest.scala
+++ src/test/scala/TileTest.scala
@@ -3,17 +3,22 @@ import chisel3.util._
 import chiseltest._
 import chiseltest.experimental.TestOptionBuilder._
 import chiseltest.internal.VerilatorBackendAnnotation
+import org.scalatest.FreeSpec
 
+import caravan.bus.common.{DummyCore}
 import caravan.bus.tilelink.{TLRequest, TLResponse, TilelinkConfig, TilelinkAdapter}
 import directmapped.DMCache
+import common.CacheConfig
 
 class TileTest extends FreeSpec with ChiselScalatestTester {
   "Tile" in {
-    implicit val config = CacheConfig(32, 32, 4)
+    implicit val config = CacheConfig(8, 32, 8, 32)
     implicit val busConfig = TilelinkConfig()
-    test(new Tile[TLRequest, TLResponse, TilelinkAdapter](new Core(), new DMCache())).withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
-      c.io.req.poke(0.U.asTypeOf(new AbstrRequest))
-      c.io.rsp.expect(0.U.asTypeOf(new AbstrResponse))
+    test(new Tile(new DummyCore(4), new TLRequest(), new TLResponse(), new TilelinkAdapter())).withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
+      // c.io.req.poke(0.U.asTypeOf(new AbstrRequest))
+      // c.io.rsp.expect(0.U.asTypeOf(new AbstrResponse))
+      c.clock.step(10)
+      c.io.rsp.poke(0.U.asTypeOf(Decoupled(new TLResponse)))
     }
   }
 }
